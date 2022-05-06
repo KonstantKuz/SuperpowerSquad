b1dd50b51a614d2f04bce7fbd68ba5976b7dc487
Anatoly Ahmedov <ludd@bk.ru>
2022-05-06 15:34:13 +0300

    add running animation

--- Assets/Scripts/Survivors/PlayerUnit/MovementController.cs
+++ Assets/Scripts/Survivors/PlayerUnit/MovementController.cs
@@ -7,7 +7,11 @@ namespace Survivors.PlayerUnit
     [RequireComponent(typeof(NavMeshAgent))]
     public class MovementController : MonoBehaviour
     {
+        private readonly int _runHash = Animator.StringToHash("Run");
+        private readonly int _idleHash = Animator.StringToHash("Idle");
+        
         private NavMeshAgent _agent;
+        private Animator _animator;
 
         [Inject] 
         private Joystick _joystick;
@@ -15,11 +19,22 @@ namespace Survivors.PlayerUnit
         private void Awake()
         {
             _agent = GetComponent<NavMeshAgent>();
+            _animator = GetComponentInChildren<Animator>();
         }
 
         private void Update()
         {
-            _agent.destination = transform.position + new Vector3(_joystick.Horizontal, 0, _joystick.Vertical);
+            if (_joystick.Direction.sqrMagnitude > 0)
+            {
+                _agent.isStopped = false;
+                _agent.destination = transform.position + new Vector3(_joystick.Horizontal, 0, _joystick.Vertical);
+                _animator.Play(_runHash);
+            }
+            else
+            {
+                _agent.isStopped = true;
+                _animator.Play(_idleHash);
+            }
         }
     }
 }
