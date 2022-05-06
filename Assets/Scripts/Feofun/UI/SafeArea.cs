b86d0b4b72cdc6f95e50204f33b82ca12a5d906b
Anatoly Ahmedov <ludd@bk.ru>
2022-05-06 16:17:36 +0300

    add safe area move location and ui in prefabs

---
+++ Assets/Scripts/Feofun/UI/SafeArea.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Feofun.UI
+{
+    public class SafeArea : MonoBehaviour
+    {
+        [SerializeField] private bool _enableTopSafeArea;
+        [SerializeField] private bool _enableBottomSafeArea;
+        private void Awake()
+        {
+            var rectTransform = GetComponent<RectTransform>();
+            var safeArea = Screen.safeArea;
+            if (_enableBottomSafeArea)
+            {
+                rectTransform.anchorMin = new Vector2(
+                safeArea.xMin / Screen.width,
+                safeArea.yMin / Screen.height);
+            }
+
+            if (_enableTopSafeArea)
+            {
+                rectTransform.anchorMax = new Vector2(
+                    safeArea.xMax / Screen.width,
+                    safeArea.yMax / Screen.height);
+            }
+        }
+    }
+}
