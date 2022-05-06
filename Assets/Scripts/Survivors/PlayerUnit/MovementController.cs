78ebec044586f231f4b3ecc6761fb639e62d528a
Anatoly Ahmedov <ludd@bk.ru>
2022-05-06 15:20:26 +0300

    control player movement

---
+++ Assets/Scripts/Survivors/PlayerUnit/MovementController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using Zenject;
+
+namespace Survivors.PlayerUnit
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class MovementController : MonoBehaviour
+    {
+        private NavMeshAgent _agent;
+
+        [Inject] 
+        private Joystick _joystick;
+
+        private void Awake()
+        {
+            _agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            _agent.destination = transform.position + new Vector3(_joystick.Horizontal, 0, _joystick.Vertical);
+        }
+    }
+}
