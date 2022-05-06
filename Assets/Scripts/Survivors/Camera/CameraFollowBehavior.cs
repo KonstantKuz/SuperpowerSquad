0768449ad943c7a98ec1ccbffc94210908a4fcf2
Anatoly Ahmedov <ludd@bk.ru>
2022-05-06 15:59:21 +0300

    camera follows player

---
+++ Assets/Scripts/Survivors/Camera/CameraFollowBehavior.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Survivors.Camera
+{
+    public class CameraFollowBehavior : MonoBehaviour
+    {
+        [SerializeField]
+        private float _distanceToObject;
+        
+        private void Update()
+        {
+            var cameraTransform = UnityEngine.Camera.main.GetComponent<Transform>();
+            cameraTransform.position = transform.position - _distanceToObject * cameraTransform.forward;
+        }
+    }
+}
