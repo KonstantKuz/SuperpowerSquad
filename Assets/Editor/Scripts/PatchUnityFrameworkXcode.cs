5d4e75236ee52faa706d78dce841c7b76d22f491
Anatoly Ahmedov <ludd@bk.ru>
2022-05-11 16:04:34 +0300

    jenkins build scripts

---
+++ Assets/Editor/Scripts/PatchUnityFrameworkXcode.cs
@@ -0,0 +1,52 @@
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+#if UNITY_EDITOR
+namespace Editor
+{
+    public class PatchUnityFrameworkXcode
+    {
+        [PostProcessBuild]
+        public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+        {
+#if UNITY_IOS            
+            SetUnityFrameworkCodeSignToManual(pathToBuiltProject);
+#endif
+        }
+
+        private static void SetUnityFrameworkCodeSignToManual(string pathToBuiltProject)
+        {
+            var projectPath = PBXProject.GetPBXProjectPath(pathToBuiltProject);
+            var project = new PBXProject();
+            project.ReadFromFile(projectPath);
+
+            FixCodeSign(project);
+            FixFirebase(project);
+
+            project.WriteToFile(projectPath);
+        }
+
+        private static void FixCodeSign(PBXProject project)
+        {
+            var unityFrameworkTarget = project.TargetGuidByName("UnityFramework");
+            project.SetBuildProperty(unityFrameworkTarget, "CODE_SIGN_STYLE", "Manual");
+            project.SetBuildProperty(unityFrameworkTarget, "CODE_SIGNING_REQUIRED", "NO");
+            project.SetBuildProperty(unityFrameworkTarget, "CODE_SIGNING_ALLOWED", "NO");
+            project.SetBuildProperty(unityFrameworkTarget, "PROVISIONING_PROFILE", "");
+            project.SetBuildProperty(unityFrameworkTarget, "EXPANDED_CODE_SIGN_IDENTITY", "");
+            project.SetBuildProperty(unityFrameworkTarget, "PROVISIONING_PROFILE_SPECIFIER", "");
+
+            var target = project.TargetGuidByName("Unity-iPhone");
+            project.SetBuildProperty(target, "CODE_SIGN_STYLE", "Manual");
+        }
+
+        private static void FixFirebase(PBXProject project)
+        {
+            var target = project.TargetGuidByName("Unity-iPhone");
+            project.SetBuildProperty(target, "OTHER_LDFLAGS", "$(inherited)");
+            project.AddBuildProperty(target, "LIBRARY_SEARCH_PATHS", "$(inherited)");
+        }
+    }
+}
+#endif
