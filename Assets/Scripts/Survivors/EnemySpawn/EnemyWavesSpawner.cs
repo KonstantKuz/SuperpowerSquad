7a90bbc2bc09b75c6e9aa519e9e37b2789ae024d
Konstantin Kuznetsov <k.kuznetsov@inspiritum.com>
2022-05-12 18:30:13 +0300

    add random position selection

--- Assets/Scripts/Survivors/EnemySpawn/EnemyWavesSpawner.cs
+++ Assets/Scripts/Survivors/EnemySpawn/EnemyWavesSpawner.cs
@@ -5,18 +5,26 @@ using Survivors.Units.Service;
 using UniRx;
 using UnityEngine;
 using Zenject;
+using Random = UnityEngine.Random;
 
 namespace Survivors.Location.EnemySpawn
 {
     public class EnemyWavesSpawner : MonoBehaviour
     {
+        [Range(0.1f, 0.3f)]
+        [SerializeField] private float _outOfViewHorizontalOffset = 0.1f;
+        [Range(0.01f, 0.05f)]
+        [SerializeField] private float _outOfViewVerticalTopOffset = 0.01f;
+        [Range(0.1f, 0.3f)]
+        [SerializeField] private float _outOfViewVerticalBottomOffset = 0.1f;
         [SerializeField] private float _timerStep = 1f;
         
         private Queue<EnemyWaveConfig> _currentMatchWaves;
         private EnemyWaveConfig _currentWave;
 
-        [Inject] private UnitFactory _unitFactory;
         [Inject] private SessionService _sessionService;
+        [Inject] private UnitFactory _unitFactory;
+        [Inject] private LocationWorld _locationWorld;
 
         private void Start()
         {
@@ -52,7 +60,36 @@ namespace Survivors.Location.EnemySpawn
 
         private Vector3 GetRandomPlaceForWave()
         {
-            return Vector3.zero;
+            var randomPointOutOfView = GetRandomPointOutOfViewport();
+            var outOfViewRay =  UnityEngine.Camera.main.ViewportPointToRay(randomPointOutOfView);
+            var plane = new Plane(_locationWorld.Player.up, _locationWorld.Player.position);
+            plane.Raycast(outOfViewRay, out var intersectionDist);
+            return outOfViewRay.GetPoint(intersectionDist);
+        }
+
+        private Vector2 GetRandomPointOutOfViewport()
+        {
+            var placeAlongVertical = GetRandomSign() > 0;
+            var horizontalValue = GetHorizontalViewportValue(placeAlongVertical);
+            var verticalValue = GetVerticalViewportValue(!placeAlongVertical);
+            return new Vector2(horizontalValue,verticalValue);
+        }
+
+        private float GetRandomSign()
+        {
+            return Random.value > 0.5f ? 1 : -1;
+        }
+
+        private float GetHorizontalViewportValue(bool inBounds)
+        {
+            return inBounds ? Random.Range(0f, 1f) : 
+                GetRandomSign() > 0 ? 1f + _outOfViewHorizontalOffset : -_outOfViewHorizontalOffset;
+        }
+
+        private float GetVerticalViewportValue(bool inBounds)
+        {
+            return inBounds ? Random.Range(0f, 1f) : 
+                GetRandomSign() > 0 ? 1f + _outOfViewVerticalTopOffset : -_outOfViewVerticalBottomOffset;
         }
 
         private void SpawnEnemy(Vector3 place)
--- Assets/Scripts/Survivors/Location/LocationWorld.cs
+++ Assets/Scripts/Survivors/Location/LocationWorld.cs
@@ -4,7 +4,10 @@ namespace Survivors.Location
 {
     public class LocationWorld : MonoBehaviour
     {
+        [SerializeField] private Transform _player;
         [SerializeField] private GameObject _spawnContainer;
+
+        public Transform Player => _player;
         public GameObject SpawnContainer => _spawnContainer;
     }
 }
