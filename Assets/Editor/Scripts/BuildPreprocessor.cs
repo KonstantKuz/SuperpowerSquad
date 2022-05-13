6bd6095aa76b6bc76eb39b5945ed51eed153e407
Anatoly Ahmedov <ludd@bk.ru>
2022-05-13 11:29:44 +0300

    move works with define symbols in separate class

--- Assets/Editor/Scripts/BuildPreprocessor.cs
+++ Assets/Editor/Scripts/BuildPreprocessor.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using UnityEditor;
-
-namespace Editor.Scripts
+﻿namespace Editor.Scripts
 {
     public static class BuildPreprocessor
     {
@@ -16,50 +12,12 @@ namespace Editor.Scripts
         {
             if (value)
             {
-                AddDefineSymbol(DEBUG_CONSOLE_DEFINE);
+                DefineSymbolsUtil.Add(DEBUG_CONSOLE_DEFINE);
             }
             else
             {
-                RemoveDefineSymbol(DEBUG_CONSOLE_DEFINE);
+                DefineSymbolsUtil.Remove(DEBUG_CONSOLE_DEFINE);
             }
-        }
-
-        private static void AddDefineSymbol(string symbol)
-        {
-            var current = GetDefineSymbols();
-            if (current.Contains(symbol))
-            {
-                return;
-            }
-
-            current.Add(symbol);
-            SetDefineSymbols(current);
-        }
-
-        private static void RemoveDefineSymbol(string symbol)
-        {
-            var current = GetDefineSymbols();
-            if (!current.Contains(symbol))
-            {
-                return;
-            }
-
-            current.Remove(symbol);
-            SetDefineSymbols(current);
-        }
-
-        private static List<string> GetDefineSymbols()
-        {
-            string definesString = PlayerSettings.GetScriptingDefineSymbolsForGroup(
-                EditorUserBuildSettings.selectedBuildTargetGroup);
-            return definesString.Split(';').ToList();
-        }
-
-        private static void SetDefineSymbols(List<string> symbols)
-        {
-            PlayerSettings.SetScriptingDefineSymbolsForGroup(
-                EditorUserBuildSettings.selectedBuildTargetGroup, 
-                string.Join(";", symbols.ToArray()));
         }
     }
 }
