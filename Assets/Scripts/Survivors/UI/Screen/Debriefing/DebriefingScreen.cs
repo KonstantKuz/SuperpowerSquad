895f6c62e8127e30e3284da04f2e9ca259f92e80
i.petrenko <i.petrenko@inspiritum.com>
2022-05-18 13:54:55 +0300

    added DebriefingScreen

---
+++ Assets/Scripts/Survivors/UI/Screen/Debriefing/DebriefingScreen.cs
@@ -0,0 +1,30 @@
+﻿using Feofun.UI.Components;
+using Feofun.UI.Screen;
+using Survivors.UI.Screen.World;
+using UnityEngine;
+using Zenject;
+
+namespace Survivors.UI.Screen.Debriefing
+{
+    public class DebriefingScreen : BaseScreen
+    {
+        private const ScreenId DEBRIEFING_SCREEN = ScreenId.Debriefing;
+        public override ScreenId ScreenId => DEBRIEFING_SCREEN; 
+        public override string Url => ScreenName;
+        
+        [SerializeField]
+        private ActionButton _nextButton;
+        [Inject]
+        private ScreenSwitcher _screenSwitcher;
+        
+        public void OnEnable()
+        {
+            _nextButton.Init(OnReload);
+        }
+
+        private void OnReload()
+        {
+            _screenSwitcher.SwitchTo(WorldScreen.WORLD_SCREEN.ToString());
+        }
+    }
+}
