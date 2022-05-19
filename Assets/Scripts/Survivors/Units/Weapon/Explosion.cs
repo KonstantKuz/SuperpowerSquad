d44e9e11050c5f4a92aa355f5e5499842c67b3e6
Anatoly Ahmedov <ludd@bk.ru>
2022-05-19 11:54:08 +0300

    add rocket to rocket unit

---
+++ Assets/Scripts/Survivors/Units/Weapon/Explosion.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using Survivors.Location.Service;
+using Survivors.Units.Component.Health;
+using Survivors.Units.Target;
+using UnityEngine;
+
+namespace Survivors.Units.Weapon
+{
+    public class Explosion : MonoBehaviour
+    {
+        [SerializeField]
+        private ParticleSystem[] _particles;
+
+        private void Explode(float damageRadius, UnitType targetType, Action<GameObject> hitCallback)
+        {
+            PlayParticles();
+            var hits = GetHits(damageRadius, targetType);
+            DamageHits(hits, hitCallback);
+        }
+
+        private void PlayParticles()
+        {
+            foreach (var ps in _particles)
+            {
+                ps.Play();
+            }
+        }
+
+        private Collider[] GetHits(float damageRadius, UnitType targetType)
+        {
+            var hits = Physics.OverlapSphere(transform.position, damageRadius);
+            return hits.Where(go => go.GetComponent<ITarget>() != null && go.GetComponent<ITarget>().IsAlive
+                                    && go.GetComponent<ITarget>().UnitType == targetType)
+                       .ToArray();
+        }
+
+        private void DamageHits(Collider[] hits, Action<GameObject> hitCallback)
+        {
+            foreach (var hit in hits) {
+                if (hit.TryGetComponent(out IDamageable _)) {
+                    hitCallback?.Invoke(hit.gameObject);
+                }
+            }
+        }
+
+        public static void Explode(WorldObjectFactory objectFactory, 
+            Explosion prefab, 
+            Vector3 pos,
+            float radius, 
+            UnitType targetType,
+            Action<GameObject> hitCallback)
+        {
+            var explosion = objectFactory.CreateObject(prefab.gameObject).GetComponent<Explosion>();
+            explosion.transform.position = pos;
+            explosion.Explode(radius, targetType, hitCallback);            
+        }
+    }
+}
---
+++ Assets/Scripts/Survivors/Units/Weapon/Projectiles/Rocket.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Survivors.Location.Service;
+using Survivors.Units.Target;
+using UnityEngine;
+using UnityEngine.Assertions;
+using Zenject;
+
+namespace Survivors.Units.Weapon.Projectiles
+{
+    public sealed class Rocket : Projectile
+    {
+        [SerializeField] 
+        private bool _followTarget;
+        [SerializeField] 
+        private float _rotationSpeed;
+        [SerializeField] 
+        private float _maxLifeTime;
+        [SerializeField]
+        private Explosion _explosion;
+        [SerializeField] 
+        private float _selfExplodeRange;
+        [SerializeField] 
+        private float _initialCourseTime;
+        
+        [Inject]
+        private WorldObjectFactory _objectFactory;
+
+        private ITarget _target;
+        private Vector3 _lastTargetPos;
+
+        private float TimeLeft { get; set; }
+
+        public override void Launch(ITarget target, ProjectileParams projectileParams, Action<GameObject> hitCallback)
+        {
+            base.Launch(target, projectileParams, hitCallback);
+            SetTarget(target);
+            TimeLeft = _maxLifeTime;
+        }
+
+        private void SetTarget(ITarget target)
+        {
+            Assert.IsNotNull(target);
+            if (!_followTarget)
+            {
+                _lastTargetPos = target.Center.position;
+                return;
+            }
+
+            if (_target == target) return;
+            if (_target != null)
+            {
+                ClearTarget();
+            }
+            
+            _target = target;
+            _target.OnTargetInvalid += ClearTarget;
+        }
+
+        private void Update()
+        {
+            UpdateTargetPos();
+            UpdatePosition();
+            
+            TimeLeft -= Time.deltaTime;
+            if (TimeLeft <= 0 || Vector3.Distance(transform.position, _lastTargetPos) < _selfExplodeRange) {
+                Explode(transform.position);
+            }
+        }
+
+        private void UpdateTargetPos()
+        {
+            if (!_followTarget) return;
+            if (!(_target is { IsAlive: true })) return;
+            _lastTargetPos = _target.Center.position;
+        }
+
+        private void UpdatePosition()
+        {
+            transform.position += transform.forward * Speed * Time.deltaTime;
+
+            if (LifeTime >= _initialCourseTime)
+            {
+                var lookRotation = Quaternion.LookRotation(_lastTargetPos - transform.position);
+                transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * _rotationSpeed);
+            }
+        }
+
+        private float LifeTime => _maxLifeTime - TimeLeft;
+
+        protected override void TryHit(GameObject target, Vector3 hitPos, Vector3 collisionNorm)
+        {
+            Explode(hitPos);
+        }
+        private void Explode(Vector3 pos)
+        {
+            Explosion.Explode(_objectFactory, _explosion, pos, Params.DamageRadius, TargetType, HitCallback);
+            Destroy();
+        }
+
+        private void Destroy()
+        {
+            ClearTarget();
+            HitCallback = null;
+            Destroy(gameObject);
+        }
+
+        private void ClearTarget()
+        {
+            if (_target != null)
+            {
+                _target.OnTargetInvalid -= ClearTarget;
+            }
+            _target = null;
+        }
+    }
+}
