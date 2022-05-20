839c63418231485fb6c6167644b35153d2befbfc
i.petrenko <i.petrenko@inspiritum.com>
2022-05-20 15:56:58 +0300

    changed fire beam

--- Assets/Scripts/Survivors/Units/Weapon/BeamWeapon.cs
+++ Assets/Scripts/Survivors/Units/Weapon/BeamWeapon.cs
@@ -10,21 +10,30 @@ namespace Survivors.Units.Weapon
     public class BeamWeapon : BaseWeapon
     {
         [SerializeField]
+        private Transform _barrel;
+        [SerializeField]
         private Beam _beam;
         [Inject]
         private WorldObjectFactory _objectFactory;
 
+        private Vector3 _barrelPos;
+
         public override void Fire(ITarget target, ProjectileParams projectileParams, Action<GameObject> hitCallback)
         {
             var beam = CreateBeam();
-            var rotationToTarget = RangedWeapon.GetShootRotation(BarrelPos, target.Center.position);
-            beam.transform.SetPositionAndRotation(BarrelPos, rotationToTarget);
-            beam.Launch(target, projectileParams, hitCallback, Barrel);
+            var rotationToTarget = RangedWeapon.GetShootRotation(_barrelPos, target.Center.position);
+            beam.transform.SetPositionAndRotation(_barrelPos, rotationToTarget);
+            beam.Launch(target, projectileParams, hitCallback, _barrel);
         }
 
         private Beam CreateBeam()
         {
             return _objectFactory.CreateObject(_beam.gameObject).GetComponent<Beam>();
+        }
+
+        private void LateUpdate()
+        {
+            _barrelPos = _barrel.position;
         }
     }
 }
--- Assets/Scripts/Survivors/Units/Weapon/RangedWeapon.cs
+++ Assets/Scripts/Survivors/Units/Weapon/RangedWeapon.cs
@@ -10,15 +10,21 @@ namespace Survivors.Units.Weapon
     public class RangedWeapon : BaseWeapon
     {
         [SerializeField]
+        private Transform _barrel;
+
+        [SerializeField]
         private Projectile _ammo;
+
+        private Vector3 _barrelPos; //Seems that in some cases unity cannot correctly take position inside animation event
+
         [Inject]
         private WorldObjectFactory _objectFactory;
 
         public override void Fire(ITarget target, ProjectileParams projectileParams, Action<GameObject> hitCallback)
         {
             var projectile = CreateProjectile();
-            var rotationToTarget = GetShootRotation(BarrelPos, target.Center.position);
-            projectile.transform.SetPositionAndRotation(BarrelPos, rotationToTarget);
+            var rotationToTarget = GetShootRotation(_barrelPos, target.Center.position);
+            projectile.transform.SetPositionAndRotation(_barrelPos, rotationToTarget);
             projectile.Launch(target, projectileParams, hitCallback);
         }
 
@@ -36,6 +42,11 @@ namespace Survivors.Units.Weapon
         private Projectile CreateProjectile()
         {
             return _objectFactory.CreateObject(_ammo.gameObject).GetComponent<Projectile>();
+        }
+
+        private void LateUpdate()
+        {
+            _barrelPos = _barrel.position;
         }
     }
 }
