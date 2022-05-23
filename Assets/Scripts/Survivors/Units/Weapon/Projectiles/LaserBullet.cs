ce0ab55ca8088e1f0e98027894e745df6f32dd54
i.petrenko <i.petrenko@inspiritum.com>
2022-05-23 19:02:26 +0300

    fixed laser bullet

--- Assets/Scripts/Survivors/Units/Weapon/Projectiles/LaserBullet.cs
+++ Assets/Scripts/Survivors/Units/Weapon/Projectiles/LaserBullet.cs
@@ -1,9 +1,26 @@
-﻿using UnityEngine;
+﻿using Survivors.Units.Component.Health;
+using Survivors.Units.Target;
+using UnityEngine;
 
 namespace Survivors.Units.Weapon.Projectiles
 {
     public class LaserBullet : Bullet
     {
+        public void OnTriggerEnter(Collider other)
+        {
+            var colliderTarget = other.GetComponent<ITarget>();
+            if (colliderTarget == null) {
+                return;
+            }
+            if (TargetType != colliderTarget.UnitType) {
+                return;
+            }
+            if (!other.TryGetComponent(out IDamageable damageable)) {
+                return;
+            }
+            TryHit(other.gameObject, transform.position, -transform.forward);
+        }
+        
         protected override void TryHit(GameObject target, Vector3 hitPos, Vector3 collisionNorm)
         {
             HitCallback?.Invoke(target);
