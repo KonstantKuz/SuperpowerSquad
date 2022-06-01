2e0b4d05add518f86ec24d023ee36f162c032d78
Konstantin Kuznetsov <k.kuznetsov@inspiritum.com>
2022-06-01 11:16:59 +0300

    refactor

--- Assets/Scripts/Survivors/Units/Weapon/RangeConeRenderer.cs
+++ Assets/Scripts/Survivors/Units/Weapon/RangeConeRenderer.cs
@@ -13,20 +13,11 @@ namespace Survivors.Units.Weapon
     {
         private const int MIN_SEGMENTS_COUNT = 2;
         private const int DEGREES_PER_SEGMENT = 3;
-        private const float DIRECTION_CORRECTION_ANGLE = 90f;
         
         [SerializeField] private Material _material;
 
-        private float _segments;
-        private float _segmentAngle;
-        private float _actualAngle;
-
         private Mesh _mesh;
-        private Vector3[] _verts;
-        private Vector3[] _normals;
-        private int[] _triangles;
-        private Vector2[] _uvs;
-
+        
         private void Awake()
         {
             var meshFilter = gameObject.RequireComponent<MeshFilter>();
@@ -45,76 +36,71 @@ namespace Survivors.Units.Weapon
 
         public void Build(float angle, float radius)
         {
-            CalculateSegments(angle);
-            PrepareMeshData();
-            CalculateVerts(angle, radius);
-            CalculateTriangles();
-            SetUvs();
-            UpdateMesh();
+            var segmentsCount = CalculateSegments(angle);
+
+            _mesh.Clear();
+            _mesh.vertices = BuildVertices(segmentsCount, angle, radius);
+            _mesh.triangles = BuildTriangles(segmentsCount);
+            _mesh.normals = BuildNormals(segmentsCount);
+            _mesh.uv = BuildUvs(_mesh.vertices);
         }
 
-        private void CalculateSegments(float angle)
+        private int CalculateSegments(float angle)
         {
-            _segments = Mathf.Max(MIN_SEGMENTS_COUNT, angle / DEGREES_PER_SEGMENT);
-            _segmentAngle = angle / _segments;
+            return (int) Mathf.Max(MIN_SEGMENTS_COUNT, angle / DEGREES_PER_SEGMENT);
         }
 
-        private void PrepareMeshData()
+        private Vector3[] BuildVertices(int segmentsCount, float angle, float radius)
         {
-            _verts = new Vector3[(int) _segments * 3];
-            _normals = new Vector3[(int) _segments * 3];
-            _triangles = new int[(int) _segments * 3];
-            _uvs = new Vector2[(int) _segments * 3];
-            for (int i = 0; i < _verts.Length; i++)
+            var vertices = new Vector3[segmentsCount * 3];
+            var segmentAngle = angle / segmentsCount;
+            var currentAngle = -angle / 2;
+            for (int i = vertices.Length - 1; i >= 0; i -= 3)
             {
-                _verts[i] = new Vector3(0, 0, 0);
-                _normals[i] = Vector3.up;
+                vertices[i] = GetVertPositionAtAngle(currentAngle, radius);
+                currentAngle += segmentAngle;
+                vertices[i - 1] = GetVertPositionAtAngle(currentAngle, radius);
             }
-        }
-
-        private void CalculateVerts(float angle, float radius)
-        {
-            var currentAngle = DIRECTION_CORRECTION_ANGLE - angle / 2;
-            for (int i = 1; i < _verts.Length; i += 3)
-            {
-                _verts[i] = GetVertPositionAtAngle(currentAngle, radius);
-                currentAngle += _segmentAngle;
-                _verts[i + 1] = GetVertPositionAtAngle(currentAngle, radius);
-            }
+            return vertices;
         }
 
         private Vector3 GetVertPositionAtAngle(float angle, float radius)
         {
-            var x = Mathf.Cos(Mathf.Deg2Rad * angle) * radius;
-            var z = Mathf.Sin(Mathf.Deg2Rad * angle) * radius;
+            var x = Mathf.Sin(Mathf.Deg2Rad * angle) * radius;
+            var z = Mathf.Cos(Mathf.Deg2Rad * angle) * radius;
             return new Vector3(x, 0, z);
         }
 
-        private void CalculateTriangles()
+        private int[] BuildTriangles(int segmentsCount)
         {
-            for (int i = 0; i < _triangles.Length; i += 3)
+            var triangles = new int[segmentsCount * 3];
+            for (int i = 0; i < triangles.Length; i += 3)
             {
-                _triangles[i] = 0;
-                _triangles[i + 1] = i + 2;
-                _triangles[i + 2] = i + 1;
+                triangles[i] = 0;
+                triangles[i + 1] = i + 2;
+                triangles[i + 2] = i + 1;
             }
+            return triangles;
         }
 
-        private void SetUvs()
+        private Vector3[] BuildNormals(int segmentsCount)
         {
-            for (int i = 0; i < _uvs.Length; i++)
+            var normals = new Vector3[segmentsCount * 3];
+            for (int i = 0; i < normals.Length; i++)
             {
-                _uvs[i] = new Vector2(_verts[i].x, _verts[i].z);
+                normals[i] = Vector3.up;
             }
+            return normals;
         }
 
-        private void UpdateMesh()
+        private Vector2[] BuildUvs(Vector3[] vertices)
         {
-            _mesh.Clear();
-            _mesh.vertices = _verts;
-            _mesh.normals = _normals;
-            _mesh.triangles = _triangles;
-            _mesh.uv = _uvs;
+            var uvs = new Vector2[vertices.Length];
+            for (int i = 0; i < uvs.Length; i++)
+            {
+                uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
+            }
+            return uvs;
         }
     }
 }
