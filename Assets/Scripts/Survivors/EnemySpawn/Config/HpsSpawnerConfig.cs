091e4350ce0ee574deb4c5f8c981f19231211155
Anatoly Ahmedov <ludd@bk.ru>
2022-06-03 15:24:57 +0300

    add hsp spawner config

---
+++ Assets/Scripts/Survivors/EnemySpawn/Config/HpsSpawnerConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+using Feofun.Config;
+using Feofun.Config.Csv;
+
+namespace Survivors.EnemySpawn.Config
+{
+    public class HpsSpawnerConfig
+    {
+        [DataMember]
+        public float StartingHPS;
+        [DataMember]
+        public float HPSSpeed;
+        [DataMember]
+        public float MinWaveSize;
+        [DataMember]
+        public float MaxWaveSize;
+        [DataMember]
+        public float MinInterval;
+        [DataMember]
+        public float MaxInterval;
+    }
+
+    public class HpsSpawnerConfigLoader : ILoadableConfig
+    {
+        public HpsSpawnerConfig Config { get; private set; }
+        
+        public void Load(Stream stream)
+        {
+            Config = new CsvSerializer().ReadSingleObject<HpsSpawnerConfig>(stream);
+        }
+    }
+}
