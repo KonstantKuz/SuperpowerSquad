b8c6bea8cb820a1ba01a59f47144ba9fc4f5726e
Konstantin Kuznetsov <k.kuznetsov@inspiritum.com>
2022-06-06 21:30:19 +0300

    add customizeable flame

--- Assets/Scripts/Survivors/UI/Screen/World/RandomUpgradeButton.cs
+++ Assets/Scripts/Survivors/UI/Screen/World/RandomUpgradeButton.cs
@@ -1,4 +1,9 @@
-﻿using Survivors.Squad.Upgrade;
+﻿using Feofun.Config;
+using Feofun.Modifiers;
+using Survivors.Modifiers.Config;
+using Survivors.Squad.Upgrade;
+using Survivors.Squad.Upgrade.Config;
+using Survivors.Units.Service;
 using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
@@ -8,7 +13,10 @@ namespace Survivors.UI.Screen.World
     [RequireComponent(typeof(Button))]
     public class RandomUpgradeButton : MonoBehaviour
     {
+        [Inject] private Location.World _world;
+        [Inject] private ModifierFactory _modifierFactory;
         [Inject] private UpgradeService _upgradeService;
+        [Inject] private StringKeyedConfigCollection<ParameterUpgradeConfig> _modifierConfigs;
         private void Awake()
         {
             GetComponent<Button>().onClick.AddListener(AddRandomUpgrade);
@@ -17,6 +25,21 @@ namespace Survivors.UI.Screen.World
         private void AddRandomUpgrade()
         {
             _upgradeService.AddRandomUpgrade();
+            // UpgradePyroUnit();
+        }
+
+        private void UpgradePyroUnit()
+        {
+            AddModifier("DamageAngle5", "PyroUnit");
+            AddModifier("AttackDistance1", "PyroUnit");
+            AddModifier("ProjectileSpeed25", "PyroUnit");
+        }
+        
+        private void AddModifier(string modifierId, string unitId)
+        {
+            var modifierConfig = _modifierConfigs.Get(modifierId);
+            var modifier = _modifierFactory.Create(modifierConfig.ModifierConfig);
+            _world.Squad.AddModifier(modifier, modifierConfig.Target, unitId);
         }
     }
 }
--- Assets/Scripts/Survivors/Units/Weapon/Projectiles/FireBeam.cs
+++ Assets/Scripts/Survivors/Units/Weapon/Projectiles/FireBeam.cs
@@ -1,22 +1,84 @@
 ﻿using System;
+using System.Collections;
+using SuperMaxim.Core.Extensions;
+using Survivors.Extension;
 using Survivors.Units.Component.Health;
 using Survivors.Units.Target;
+using Survivors.Units.Weapon.Projectiles.Params;
 using UnityEngine;
 
 namespace Survivors.Units.Weapon.Projectiles
 {
-    public class FireBeam : Beam
+    public class FireBeam : Projectile
     {
-        protected override void TryHit(GameObject target)
+        [SerializeField] private float _emissionCountMultiplier = 0.4f;
+        [SerializeField] private float _flameThrowDuration = 0.3f;
+        [SerializeField] private float _flameLifeTimeMultiplier = 1.1f;
+        [SerializeField] private float _damagePeriodMultiplier = 0.2f;
+        [SerializeField] private float _destroyDelay = 1f;
+        [SerializeField] private ParticleSystem[] _flameParticles;
+
+        private float FlameLifeTime => _flameLifeTimeMultiplier * Params.AttackDistance / Speed;
+        private float FlameWidth => _flameThrowDuration * Speed;
+        private float DamagePeriod => _damagePeriodMultiplier * FlameWidth / Speed;
+        
+        public override void Launch(ITarget target, IProjectileParams projectileParams, Action<GameObject> hitCallback)
         {
-            TryHitTargetsInCone();
+            base.Launch(target, projectileParams, hitCallback);
+            _flameParticles.ForEach(UpdateFlameParams);
+            _flameParticles.ForEach(it => it.Play());
+            StartCoroutine(BurnTargets());
         }
 
-        private void TryHitTargetsInCone()
+        private void UpdateFlameParams(ParticleSystem flame)
         {
-            var hits = Projectile.GetHits(Barrel.position, ProjectileParams.AttackDistance, TargetType);
-            foreach (var hit in hits) {
-                if (!IsTargetInsideCone(hit.transform.position, Barrel.position, Barrel.forward, ProjectileParams.DamageAngle)) {
+            var mainModule = flame.main;
+            mainModule.startSpeed = Speed;
+            mainModule.startLifetime = FlameLifeTime;
+            mainModule.duration = _flameThrowDuration;
+            var shapeModule = flame.shape;
+            shapeModule.angle = Params.DamageAngle / 2;
+            var emissionModule = flame.emission;
+            emissionModule.rateOverTime = Params.DamageAngle * Params.AttackDistance * Speed * _emissionCountMultiplier;
+        }
+
+        private IEnumerator BurnTargets()
+        {
+            var lifeTime = 0f;
+            var flameStartRadius = 0f;
+            var flameEndRadius = 0f;
+            var damageTimer = 0f;
+            while (lifeTime < FlameLifeTime)
+            {
+                var deltaTime = Time.deltaTime;
+                lifeTime += deltaTime;
+                flameEndRadius += deltaTime * Speed;
+                if (lifeTime > _flameThrowDuration)
+                {
+                    flameStartRadius += deltaTime * Speed;
+                }
+
+                damageTimer += deltaTime;
+                if (damageTimer > DamagePeriod)
+                {
+                    damageTimer = 0;
+                    TryHitTargetsInFlame(flameStartRadius, flameEndRadius);
+                }
+                
+                yield return null;
+            }
+            yield return new WaitForSeconds(_destroyDelay);
+            Destroy(gameObject);
+        }
+
+        private void TryHitTargetsInFlame(float flameStartRadius, float flameEndRadius)
+        {
+            var hits = GetHits(transform.position, flameEndRadius, TargetType);
+            foreach (var hit in hits)
+            {
+                if (!IsTargetInsideCone(hit.transform.position, transform.position, transform.forward, Params.DamageAngle)
+                || !IsTargetInsideDistanceRange(hit.transform.position, transform.position, flameStartRadius, flameEndRadius)) 
+                {
                     continue;
                 }
                 if (hit.TryGetComponent(out IDamageable damageable)) {
@@ -24,11 +86,18 @@ namespace Survivors.Units.Weapon.Projectiles
                 }
             }
         }
+        
         private static bool IsTargetInsideCone(Vector3 target, Vector3 coneOrigin, Vector3 coneDirection, float maxAngle)
         {
             var targetDirection = target - coneOrigin;
-            var angle = Vector3.Angle(coneDirection, targetDirection);
+            var angle = Vector3.Angle(coneDirection, targetDirection.XZ());
             return angle <= maxAngle;
+        }
+
+        private static bool IsTargetInsideDistanceRange(Vector3 target, Vector3 origin, float distanceMin, float distanceMax)
+        {
+            var distance = Vector3.Distance(origin, target);
+            return distance > distanceMin && distance < distanceMax;
         }
     }
 }
