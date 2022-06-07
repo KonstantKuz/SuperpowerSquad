88ccc3c682f9760e09e90832b9047f73b3edad13
Konstantin Kuznetsov <k.kuznetsov@inspiritum.com>
2022-06-07 16:56:09 +0300

    remove unused fields

--- Assets/Scripts/Survivors/UI/Screen/World/RandomUpgradeButton.cs
+++ Assets/Scripts/Survivors/UI/Screen/World/RandomUpgradeButton.cs
@@ -11,10 +11,8 @@ namespace Survivors.UI.Screen.World
     [RequireComponent(typeof(Button))]
     public class RandomUpgradeButton : MonoBehaviour
     {
-        [Inject] private Location.World _world;
-        [Inject] private ModifierFactory _modifierFactory;
         [Inject] private UpgradeService _upgradeService;
-        [Inject] private StringKeyedConfigCollection<ParameterUpgradeConfig> _modifierConfigs;
+
         private void Awake()
         {
             GetComponent<Button>().onClick.AddListener(AddRandomUpgrade);
