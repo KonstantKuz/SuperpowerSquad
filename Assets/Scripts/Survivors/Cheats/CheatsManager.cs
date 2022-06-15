c89dded4517a426b7e5ebf3b47f6a9e7151cbe56
i.petrenko <i.petrenko@inspiritum.com>
2022-06-15 13:42:18 +0300

    refactoring CheatsManager

--- Assets/Scripts/Survivors/Cheats/CheatsManager.cs
+++ Assets/Scripts/Survivors/Cheats/CheatsManager.cs
@@ -1,6 +1,8 @@
 using System;
 using Feofun.Localization.Service;
 using Survivors.Cheats.Repository;
+using Survivors.Squad.Service;
+using Survivors.Squad.Upgrade;
 using UnityEngine;
 using Zenject;
 
@@ -9,45 +11,55 @@ namespace Survivors.Cheats
     public class CheatsManager : MonoBehaviour
     {
         private readonly CheatRepository _repository = new CheatRepository();
-
-
-
-        [Inject] private LocalizationService _localizationService;
-        [Inject] private DiContainer _container;
+        
+        [Inject] private LocalizationService _localizationService;     
+        [Inject] private SquadProgressService _squadProgressService;
+        [Inject] private UpgradeService _upgradeService;
 
         [SerializeField] private GameObject _fpsMonitor;
         [SerializeField] private GameObject _debugConsole;
-
- 
+        
         private CheatSettings Settings => _repository.Get() ?? new CheatSettings();
         
         private void Awake()
         {
-            _debugConsole.SetActive(IsConsoleEnabled);
 #if DEBUG_CONSOLE_ENABLED
-            _debugConsole.SetActive(true);
+            IsConsoleEnabled = true;
 #endif
+            _debugConsole.SetActive(IsConsoleEnabled); 
+            _fpsMonitor.SetActive(IsFPSMonitorEnabled);
+
         }
         public void ResetProgress()
         {
             PlayerPrefs.DeleteAll();
+            Application.Quit();
         }
         
-        public void AddExp(int amount)
-        {
-            _progressService.AddExp(amount);
-            
-        }
+        public void IncreaseSquadLevel() => _squadProgressService.IncreaseLevel();
+        private void AddRandomSquadUpgrade() => _upgradeService.AddRandomUpgrade();
+     
         public void SetLanguage(string language)
         {
             _localizationService.SetLanguageOverride(language);
         }
-        
-        public void ToggleDebugConsole() => _debugConsole.SetActive(!_debugConsole.activeInHierarchy);
-        public void ToggleFPSMonitor() => _fpsMonitor.SetActive(!_fpsMonitor.activeInHierarchy);
-        
-        
-        public bool IsConsoleEnabled
+        public void ToggleDebugConsole()
+        {
+            IsConsoleEnabled = !isActiveAndEnabled;
+            _debugConsole.SetActive(IsConsoleEnabled);
+        }
+        public void ToggleFPSMonitor()
+        {
+            IsFPSMonitorEnabled = !IsFPSMonitorEnabled;
+            _fpsMonitor.SetActive(IsFPSMonitorEnabled);
+        }
+        private void UpdateSettings(Action<CheatSettings> updateFunc)
+        {
+            var settings = Settings;
+            updateFunc?.Invoke(settings);
+            _repository.Set(settings);
+        }
+        private bool IsConsoleEnabled
         {
             get => Settings.ConsoleEnabled;
             set
@@ -55,19 +67,13 @@ namespace Survivors.Cheats
                 UpdateSettings(settings => { settings.ConsoleEnabled = value; });
             }
         }    
-        public bool IsFPSMonitorEnabled
+        private bool IsFPSMonitorEnabled
         {
-            get => Settings.ConsoleEnabled;
+            get => Settings.FPSMonitorEnabled;
             set
             {
-                UpdateSettings(settings => { settings.ConsoleEnabled = value; });
+                UpdateSettings(settings => { settings.FPSMonitorEnabled = value; });
             }
-        }
-        private void UpdateSettings(Action<CheatSettings> updateFunc)
-        {
-            var settings = Settings;
-            updateFunc?.Invoke(settings);
-            _repository.Set(settings);
         }
     }
 }
