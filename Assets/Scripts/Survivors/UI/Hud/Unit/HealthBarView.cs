b711dab273b208bcb639f8c986b1cf9c56951f8f
i.petrenko <i.petrenko@inspiritum.com>
2022-06-16 18:07:31 +0300

    refactoring

--- Assets/Scripts/Survivors/UI/Hud/Unit/HealthBarView.cs
+++ Assets/Scripts/Survivors/UI/Hud/Unit/HealthBarView.cs
@@ -35,9 +35,9 @@ namespace Survivors.UI.Hud.Unit
         private void UpdateMaxValue(float maxValue)
         {
             var scaleIncrementDelta = ((maxValue - _model.StartingMaxValue) * _constantsConfig.HealthScaleIncrementFactor) / _model.StartingMaxValue;
-            var barLenght = 1 + scaleIncrementDelta;
+            var barLength = 1 + scaleIncrementDelta;
             var scale = _barContainer.localScale;
-            _barContainer.localScale = new Vector3(barLenght, scale.y, scale.z);
+            _barContainer.localScale = new Vector3(barLength, scale.y, scale.z);
         }   
 
         protected void OnDisable()
