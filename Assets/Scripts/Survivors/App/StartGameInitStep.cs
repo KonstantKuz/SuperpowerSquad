81057f4dbdef13275cf1fd97958c870db9c771a2
i.petrenko <i.petrenko@inspiritum.com>
2022-06-22 18:38:50 +0300

    fixed LoggerInitializer

--- Assets/Scripts/Survivors/App/StartGameInitStep.cs
+++ Assets/Scripts/Survivors/App/StartGameInitStep.cs
@@ -1,11 +1,9 @@
-using System;
 using Feofun.App.Init;
 using Feofun.UI.Screen;
 using JetBrains.Annotations;
 using Logger.Assets.Scripts;
 using Survivors.UI.Screen.World;
 using Survivors.Units.Enemy;
-using UnityEngine;
 using Zenject;
 using ILogger = Logger.Assets.Scripts.ILogger;
 
@@ -21,8 +19,7 @@ namespace Survivors.App
         
         protected override void Run()
         {
-            _logger.Info("Init game", new ArgumentException("MY EXP"));
-            Debug.LogException(new ArgumentException("MY EXP"));
+            _logger.Info("Init game");
             NavMeshInitializer.Init();
             _screenSwitcher.SwitchTo(WorldScreen.ID.ToString());
             Next();
