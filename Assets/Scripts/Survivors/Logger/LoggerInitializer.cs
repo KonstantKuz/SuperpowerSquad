3e583529a1d569ff842c45e56dd5a5bce4efb6e5
i.petrenko <i.petrenko@inspiritum.com>
2022-06-23 17:57:27 +0300

    fix LoggerInitializer

--- Assets/Scripts/Survivors/Logger/LoggerInitializer.cs
+++ Assets/Scripts/Survivors/Logger/LoggerInitializer.cs
@@ -5,10 +5,12 @@ namespace Survivors.Logger
 {
     public static class LoggerInitializer
     {
+        private const string LOGGER_CONFIG_PATH = "Logger/LoggerConfig";
+        
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void Configure()
         {
-            var configured = LoggerConfigurator.Configure("Logger/LoggerConfig");
+            var configured = LoggerConfigurator.Configure(LOGGER_CONFIG_PATH);
             LoggerFactory.GetLogger("LoggerInitializer").Info($"Logger has configured:= {configured}, Actiive logger:={LoggerConfigurator.ActiveLogger}");
         }
     }
