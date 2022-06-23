7fbbe57d54dfdfc03c7f97dad0d0b8e285cf58aa
Konstantin Kuznetsov <k.kuznetsov@inspiritum.com>
2022-06-23 16:32:39 +0300

    fix weapon timer

--- Assets/Scripts/Survivors/Units/Player/Attack/WeaponTimer.cs
+++ Assets/Scripts/Survivors/Units/Player/Attack/WeaponTimer.cs
@@ -6,26 +6,31 @@ namespace Survivors.Units.Player.Attack
 {
     public class WeaponTimer
     {
+        private readonly bool _useRealTime;
         private readonly IReadOnlyReactiveProperty<float> _attackInterval;
 
-        private float _timer;
+        private float _nextAttackTimer;
+        private float _lastAttackTime;
         public event Action OnAttackReady;
   
-        private bool IsAttackReady => _timer >= AttackInterval;
+        private bool IsAttackReady => _useRealTime ? Time.time >= _lastAttackTime + AttackInterval : _nextAttackTimer >= AttackInterval;
         private float AttackInterval => Math.Max(_attackInterval.Value, 0);
-        public WeaponTimer(IReadOnlyReactiveProperty<float> attackInterval)
+        public WeaponTimer(IReadOnlyReactiveProperty<float> attackInterval, bool useRealTime = true)
         {
+            _useRealTime = useRealTime;
             _attackInterval = attackInterval;
+            _lastAttackTime = Time.time - (Time.time % AttackInterval);
         }
-
         public void OnTick()
         {
-            _timer += Time.deltaTime;
+            _nextAttackTimer += Time.deltaTime;
             if (!IsAttackReady) {
                 return;
             }
             OnAttackReady?.Invoke();
-            _timer = 0f;
+            _nextAttackTimer = 0;
+            _lastAttackTime = Time.time;
         }
     }
+    
 }
