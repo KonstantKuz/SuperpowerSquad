f62b7df3db634260ddf7080489b7fec54de78275
i.petrenko <i.petrenko@inspiritum.com>
2022-06-23 10:42:55 +0300

    fixed LoggerFactory

--- Assets/Logger/Assets/Scripts/LoggerFactory.cs
+++ Assets/Logger/Assets/Scripts/LoggerFactory.cs
@@ -7,27 +7,25 @@ namespace Logger.Assets.Scripts
 {
     public static class LoggerFactory
     {
+        private static readonly UnityLogger _unityLogger = new UnityLogger();
+        
         public static ILogger GetLogger<T>()
         {
             return GetLogger(typeof(T));
         } 
         public static ILogger GetLogger(Type clazz)
         {
-            switch (LoggerConfigurator.ActiveLogger) {
-                case LoggerType.Log4Net:
-                    return new Log4NetLogger(log4net.LogManager.GetLogger(clazz), clazz);
-                default:
-                    return new UnityLogger();
-            }
+            return LoggerConfigurator.ActiveLogger switch {
+                    LoggerType.Log4Net => new Log4NetLogger(log4net.LogManager.GetLogger(clazz), clazz),
+                    _ => _unityLogger
+            };
         }
         public static ILogger GetLogger(string name)
         {
-            switch (LoggerConfigurator.ActiveLogger) {
-                case LoggerType.Log4Net:
-                    return new Log4NetLogger(log4net.LogManager.GetLogger(name), Assembly.GetCallingAssembly().GetType());
-                default:
-                    return new UnityLogger();
-            }
+            return LoggerConfigurator.ActiveLogger switch {
+                    LoggerType.Log4Net => new Log4NetLogger(log4net.LogManager.GetLogger(name), Assembly.GetCallingAssembly().GetType()),
+                    _ => _unityLogger
+            };
         }
     }
 }
