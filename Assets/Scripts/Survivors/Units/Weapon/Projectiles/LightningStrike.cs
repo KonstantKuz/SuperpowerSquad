746336c30dd5ced60009342621cb5e1ff0a8f858
Konstantin Kuznetsov <k.kuznetsov@inspiritum.com>
2022-06-24 17:21:03 +0300

    add lightning follow

--- Assets/Scripts/Survivors/Units/Weapon/Projectiles/LightningStrike.cs
+++ Assets/Scripts/Survivors/Units/Weapon/Projectiles/LightningStrike.cs
@@ -7,13 +7,21 @@ namespace Survivors.Units.Weapon.Projectiles
 {
     public class LightningStrike : Projectile
     {
+        private Transform _targetCenter;
         public override void Launch(ITarget target, IProjectileParams projectileParams, Action<GameObject> hitCallback)
         {
             base.Launch(target, projectileParams, hitCallback);
-            
-            transform.position = target.Center.position;
+
+            _targetCenter = target.Center;
+            transform.position = _targetCenter.position;
             transform.localScale *= projectileParams.DamageRadius;
             TryHit(target.Root.parent.gameObject, target.Center.position, Vector3.zero);
+        }
+
+        private void Update()
+        {
+            if(_targetCenter == null) return;
+            transform.position = _targetCenter.position;
         }
     }
 }
