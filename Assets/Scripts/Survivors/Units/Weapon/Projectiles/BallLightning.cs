54810e57bead6a9a7be65dea5b6c803f10277fc6
i.petrenko <i.petrenko@inspiritum.com>
2022-06-27 16:36:59 +0300

    refactoring BallLightning

--- Assets/Scripts/Survivors/Units/Weapon/Projectiles/BallLightning.cs
+++ Assets/Scripts/Survivors/Units/Weapon/Projectiles/BallLightning.cs
@@ -1,89 +1,106 @@
 ﻿using System;
 using Survivors.Extension;
+using Survivors.Location.Service;
 using Survivors.Units.Target;
 using Survivors.Units.Weapon.Projectiles.Params;
+using UniRx;
 using UnityEngine;
+using Zenject;
 
 namespace Survivors.Units.Weapon.Projectiles
 {
     public class BallLightning : Projectile
     {
+        [SerializeField] private AnimationCurve _animationCurve;
+
+        [SerializeField] private float _curveTimeFactor;
+        [SerializeField] private float _curveWidthFactor;
+        [SerializeField] private float _destroyDelay;
+        [SerializeField] private float _stopDistance;
+        [SerializeField] private float _meshScaleFactor;
+
+        [SerializeField] private Transform _rootContainer;
+        [SerializeField] private Transform _meshContainer;
+
+        [SerializeField] private float _lightningDuration;
+        [SerializeField] private float _hitTimeout;
+
+        [Inject]
+        private WorldObjectFactory _objectFactory;
+
+        private float _destroyTimer;
+        private float _curveTime;
         private Vector3 _targetPosition;
-        [SerializeField]
-        private AnimationCurve _animationCurve;
-        [SerializeField]
-        private float _speed;    
-        [SerializeField]
-        private float _timeFactor;   
-        [SerializeField]
-        private float _xFactor;
-        [SerializeField]
-        private float _timeBeforeDestroy;      
-        [SerializeField]
-        private float _stopDistance;    
-        [SerializeField]
-        private Transform _root;
-        
-        private float _timer;
-        private float _time;
-        private float _stratX;
-
-        private bool isStop;
-
-        private bool CanDestroy => _timer >= _timeBeforeDestroy;
+        private LightningBoltGenerator _lightningBoltGenerator;
+        private CompositeDisposable _disposable = new CompositeDisposable();
+        private bool CanDestroy => _destroyTimer >= _destroyDelay;
         private bool IsTargetPositionReached => Vector3.Distance(transform.position.XZ(), _targetPosition.XZ()) < _stopDistance;
+
         public override void Launch(ITarget target, IProjectileParams projectileParams, Action<GameObject> hitCallback)
         {
             base.Launch(target, projectileParams, hitCallback);
-
             _targetPosition = target.Root.position;
-            transform.localScale *= Params.DamageRadius;
-            _stratX = _root.localPosition.x;
+            _lightningBoltGenerator = gameObject.RequireComponent<LightningBoltGenerator>();
+            SetDamageRadius();
+            SetMeshScale();
+            Observable.Interval(TimeSpan.FromSeconds(_hitTimeout)).Subscribe(it => { TryHit(); }).AddTo(_disposable);
+        }
+
+        private void SetDamageRadius()
+        {
+            _rootContainer.gameObject.RequireComponent<SphereCollider>().radius = Params.DamageRadius;
+        }
+
+        private void SetMeshScale()
+        {
+            var meshScale = Params.DamageRadius * _meshScaleFactor;
+            _meshContainer.localScale = Vector3.one * meshScale;
+        }
+
+        private void TryHit()
+        {
+            var hits = GetHits(transform.position, Params.DamageRadius, TargetType);
+            foreach (var hit in hits) {
+                if (!CanDamageTarget(hit, TargetType, out var target)) {
+                    return;
+                }
+                _lightningBoltGenerator.Hit(_objectFactory, _rootContainer, target.Center, _lightningDuration);
+                HitCallback?.Invoke(hit.gameObject);
+            }
         }
 
         private void Update()
         {
-            if (IsTargetPositionReached)
-            {
-                _timer += Time.deltaTime;
-            } else {
+            if (!IsTargetPositionReached) {
                 UpdatePosition();
+            } else {
+                _destroyTimer += Time.deltaTime;
             }
             if (CanDestroy) {
                 Destroy();
             }
         }
+
         private void UpdatePosition()
         {
-            _time += Time.deltaTime * _speed * _timeFactor;
-            if (_time > 1) {
-                _time = 0;
-            }
-            var newVector = transform.forward * _speed * Time.deltaTime;
-            transform.position += newVector;
-            _root.localPosition = new Vector3(_stratX - _animationCurve.Evaluate(_time) * _xFactor, _root.localPosition.y, _root.localPosition.z);
-
-
+            transform.position += transform.forward * Speed * Time.deltaTime;
+            UpdateCurvePosition();
         }
+
+        private void UpdateCurvePosition()
+        {
+            _curveTime += Time.deltaTime * Speed * _curveTimeFactor;
+            _curveTime = Mathf.Clamp01(_curveTime);
+            var localPosition = _rootContainer.localPosition;
+            _rootContainer.localPosition = new Vector3(_animationCurve.Evaluate(_curveTime) * _curveWidthFactor, localPosition.y, localPosition.z);
+        }
+
         private void Destroy()
         {
             HitCallback = null;
             Destroy(gameObject);
-        }
-        public void OnTriggerEnter(Collider collider)
-        {
-            if (!CanDamageTarget(collider, TargetType, out var target)) {
-                return;
-            }
-            var targetDirection = collider.transform.position - transform.position;
-      
-
-
-            TryHitTargetsInRadius(transform.position, 
-                                  Params.DamageRadius,
-                                  TargetType,
-                                  null,
-                                  HitCallback);
+            _disposable?.Dispose();
+            _disposable = null;
         }
     }
 }
---
+++ Assets/Scripts/Survivors/Units/Weapon/Projectiles/LightningBoltGenerator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using DigitalRuby.ThunderAndLightning;
+using Survivors.Location.Service;
+using UniRx;
+using UnityEngine;
+
+namespace Survivors.Units.Weapon.Projectiles
+{
+    public class LightningBoltGenerator : MonoBehaviour
+    {
+        private struct LightningBoltData
+        {
+            public LightningBoltPrefabScript Lightning { get; set; }
+            public Transform EndPosition { get; set; }
+        }
+
+        [SerializeField]
+        private LightningBoltPrefabScript _lightningBoltPrefab;
+
+        private readonly List<LightningBoltData> _lightnings = new List<LightningBoltData>();
+        private CompositeDisposable _disposable = new CompositeDisposable();
+
+        public void Hit(WorldObjectFactory objectFactory, Transform container, Transform endPosition, float duration)
+        {
+            var lightningData = CreateLightning(objectFactory, container, endPosition);
+            _lightnings.Add(lightningData);
+            Observable.Timer(TimeSpan.FromSeconds(duration)).Subscribe(it => Destroy(lightningData)).AddTo(_disposable);
+        }
+
+        private void Destroy(LightningBoltData lightningData)
+        {
+            Destroy(lightningData.Lightning);
+            _lightnings.Remove(lightningData);
+        }
+
+        private LightningBoltData CreateLightning(WorldObjectFactory objectFactory, Transform container, Transform endPosition)
+        {
+            var lightningBolt = objectFactory.CreateObject(_lightningBoltPrefab.gameObject, container).GetComponent<LightningBoltPrefabScript>();
+            lightningBolt.transform.localPosition = Vector3.zero;
+            lightningBolt.Source.transform.localPosition = Vector3.zero;
+            lightningBolt.Destination.transform.position = endPosition.position;
+            return new LightningBoltData {
+                    Lightning = lightningBolt,
+                    EndPosition = endPosition,
+            };
+        }
+
+        private void Update()
+        {
+            _lightnings.ForEach(it => {
+                if (it.Lightning == null || it.EndPosition) {
+                    return;
+                }
+                it.Lightning.Destination.transform.position = it.EndPosition.position;
+            });
+        }
+
+        private void OnDestroy()
+        {
+            _disposable?.Dispose();
+            _disposable = null;
+        }
+    }
+}
