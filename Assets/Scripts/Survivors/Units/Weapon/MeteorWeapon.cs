67cd6c0cd4d3b01215c6243dc36c66c46a674739
Anatoly Ahmedov <ludd@bk.ru>
2022-06-27 09:48:26 +0300

    support shot count in meteor

--- Assets/Scripts/Survivors/Units/Weapon/MeteorWeapon.cs
+++ Assets/Scripts/Survivors/Units/Weapon/MeteorWeapon.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using Survivors.Extension;
 using Survivors.Location.Service;
 using Survivors.Units.Target;
@@ -7,6 +8,7 @@ using Survivors.Units.Weapon.Projectiles.Params;
 using UnityEngine;
 using UnityEngine.Assertions;
 using Zenject;
+using Random = UnityEngine.Random;
 
 namespace Survivors.Units.Weapon
 {
@@ -14,6 +16,7 @@ namespace Survivors.Units.Weapon
     {
         [SerializeField] private float _startHeight;
         [SerializeField] private Meteor _meteor;
+        [SerializeField] private float _randomDelay;
         
         [Inject]
         private WorldObjectFactory _objectFactory;
@@ -22,16 +25,46 @@ namespace Survivors.Units.Weapon
         public override void Fire(ITarget target, IProjectileParams projectileParams, Action<GameObject> hitCallback)
         {
             Assert.IsNotNull(projectileParams);
+
+            StartCoroutine(ShootMultipleWithDelay(
+                target.Root.position, 
+                target.UnitType, 
+                projectileParams,
+                hitCallback));
+        }
+
+        private IEnumerator ShootMultipleWithDelay(Vector3 targetPos, UnitType targetUnitType, IProjectileParams projectileParams, Action<GameObject> hitCallback)
+        {
+            var nextPosition = targetPos;
+            
+            for (int i = 0; i < projectileParams.Count; i++)
+            {
+                ShootAtPosition(nextPosition, targetUnitType, projectileParams, hitCallback);
+
+                yield return new WaitForSeconds(Random.Range(0, _randomDelay));
+
+                nextPosition = GetRandomTargetPosition(targetPos, projectileParams);
+            }
+        }
+
+        private void ShootAtPosition(Vector3 position, UnitType targetUnitType, IProjectileParams projectileParams,
+            Action<GameObject> hitCallback)
+        {
             var projectile = _objectFactory.CreateObject(_meteor.gameObject).RequireComponent<Meteor>();
             projectile.transform.SetPositionAndRotation(
-                target.Root.position + _startHeight * Vector3.up,
+                position + _startHeight * Vector3.up,
                 Quaternion.LookRotation(Vector3.down));
-
-            projectile.Launch(target.UnitType, 
+            
+            projectile.Launch(targetUnitType,
                 projectileParams,
                 _startHeight / projectileParams.Speed,
                 projectileParams.Speed,
                 hitCallback);
+        }
+
+        private static Vector3 GetRandomTargetPosition(Vector3 targetPos, IProjectileParams projectileParams)
+        {
+            return targetPos + Quaternion.Euler(0, Random.Range(0.0f, 360.0f), 0) * Vector3.forward * Random.Range(1.0f, 2.0f) * projectileParams.DamageRadius;
         }
     }
 }
