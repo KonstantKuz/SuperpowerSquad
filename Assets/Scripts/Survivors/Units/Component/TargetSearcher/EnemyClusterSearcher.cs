2ae04d413e10dcf1fa1bd9863028a88329993cf5
i.petrenko <i.petrenko@inspiritum.com>
2022-06-28 11:21:39 +0300

    fixed EnemyClusterSearcher

--- Assets/Scripts/Survivors/Units/Component/TargetSearcher/EnemyClusterSearcher.cs
+++ Assets/Scripts/Survivors/Units/Component/TargetSearcher/EnemyClusterSearcher.cs
@@ -11,6 +11,8 @@ namespace Survivors.Units.Component.TargetSearcher
 {
     public class EnemyClusterSearcher : MonoBehaviour, ITargetSearcher, IInitializable<IUnit>
     {
+        private const float MAX_DISTANCE_IN_CLUSTER = 1.5f;       
+        private const float UNIT_DISTANCE_OUTSIDE_GROUP = 1000;
         private IUnit _owner;
         private UnitType _targetType;
 
@@ -28,14 +30,31 @@ namespace Survivors.Units.Component.TargetSearcher
         public ITarget Find()
         {
             var targets = GetTargetsInRadius().ToList();
-            return targets.OrderBy(it => GetWeightBySumDistance(it, targets)).FirstOrDefault();
+            
+            ITarget result = null;
+            float minDistanceWeigh = float.MaxValue;
+            foreach (var target in targets)
+            {
+                if (!target.IsAlive) continue;
+                var weight = GetWeightBySumDistance(target, targets);
+                if (weight > minDistanceWeigh) continue;
+                minDistanceWeigh = weight;
+                result = target;
+            }
+            return result;
         }
         
-        private float GetWeightBySumDistance(ITarget target, IEnumerable<ITarget> targets)
+        private float GetWeightBySumDistance(ITarget researchedTarget, IEnumerable<ITarget> targets)
         {
-            return targets.Sum(it => (it.Root.position - target.Root.position).magnitude);
+            float distanceWight = 0;
+            foreach (var target in targets) {
+                var distance = (researchedTarget.Root.position - target.Root.position).magnitude; 
+                distance = distance > MAX_DISTANCE_IN_CLUSTER ? UNIT_DISTANCE_OUTSIDE_GROUP : distance;
+                distanceWight += distance;
+            }
+            return distanceWight;
         }
-
+        
         private IEnumerable<ITarget> GetTargetsInRadius()
         {
             return _targetService.GetTargetsInRadius(_owner.SelfTarget.Root.position, _targetType, SearchDistance);
