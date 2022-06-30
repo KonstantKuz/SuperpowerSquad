118ea9017a5eac927569b127c420ea9ef4f303e3
i.petrenko <i.petrenko@inspiritum.com>
2022-06-30 13:48:34 +0300

    refactoring

--- Assets/Scripts/Survivors/Reward/Model/RewardType.cs
+++ Assets/Scripts/Survivors/Reward/Model/RewardType.cs
@@ -6,19 +6,4 @@
         Currency,
 
     }
-    public static class RewardTypeExtension
-    {
-        /*public static string GetIconPath(this RewardItem rewardItem)
-        {
-            return GetIconPath(rewardItem.RewardType, rewardItem.RewardId);
-        }*/
-        /*public static string GetIconPath(RewardType rewardType, string rewardId)
-        {
-            return rewardType switch {
-                    RewardType.Shards => IconPath.GetUnitVertical(rewardId),
-                    RewardType.LootBox => IconPath.GetReward(RewardType.LootBox.ToString()),
-                    _ => IconPath.GetReward(rewardId)
-            };
-        }*/
-    }
 }
