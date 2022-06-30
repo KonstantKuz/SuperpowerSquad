26011ae524f9e10d3227991c11c9048b4d7f3bf2
i.petrenko <i.petrenko@inspiritum.com>
2022-06-30 11:45:21 +0300

    added MissionResultRewardService

---
+++ Assets/Scripts/Survivors/Reward/Service/MissionResultRewardService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Feofun.Config;
+using JetBrains.Annotations;
+using Survivors.Player.Wallet;
+using Survivors.Reward.Config;
+using Survivors.Reward.Model;
+using Survivors.Session.Model;
+
+namespace Survivors.Reward.Service
+{
+    [PublicAPI]
+    public class MissionResultRewardService
+    {
+        private readonly ConfigCollection<SessionResult, MissionRewardsConfig> _missionRewards;
+
+        public List<RewardItem> CalculateRewards(SessionResult result, Session.Model.Session session)
+        {
+            var missionConfig = _missionRewards.Get(result);
+            var rewardCount = (int) (session.Kills * missionConfig.KilledFactor);
+            var reward = new RewardItem(Currency.Soft.ToString(), RewardType.Currency, rewardCount);
+            return new List<RewardItem>() {
+                    reward
+            };
+        }
+    }
+}
