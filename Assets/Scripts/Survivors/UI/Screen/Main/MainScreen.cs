776f2e418d9c4934a8d17dca391b5719db7423ac
i.petrenko <i.petrenko@inspiritum.com>
2022-07-04 13:41:51 +0300

    todo

--- Assets/Scripts/Survivors/UI/Screen/Main/MainScreen.cs
+++ Assets/Scripts/Survivors/UI/Screen/Main/MainScreen.cs
@@ -40,7 +40,7 @@ namespace Survivors.UI.Screen.Main
             _world.Setup();
             _sessionService.Start();
         
-            _joystick.Attach(transform); // todo: can be added to the parent screen 
+            _joystick.Attach(transform); // todo: can be replaced by adding to the parent screen
             _levelProgressBar.Init(_sessionService.LevelId + 1);
             
             StartCoroutine(WaitForAnimationUpdateBeforePause());
