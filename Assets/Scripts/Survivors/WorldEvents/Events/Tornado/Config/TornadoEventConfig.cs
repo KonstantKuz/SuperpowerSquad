58ff98b406b95f53ecd6f6611cb172e2ecf4a547
i.petrenko <i.petrenko@inspiritum.com>
2022-08-19 13:32:01 +0300

    added TornadoEventConfig

---
+++ Assets/Scripts/Survivors/WorldEvents/Events/Tornado/Config/TornadoEventConfig.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Survivors.WorldEvents.Events.Tornado.Config
+{
+    [CreateAssetMenu(fileName = "TornadoEventConfig", menuName = "ScriptableObjects/EventConfig/TornadoEventConfig")]
+    public class TornadoEventConfig : EventConfig
+    {
+        [SerializeField]
+        private int _spawnCountOnCircle = 5;
+        [SerializeField]
+        public int _spawnCountStepOnCircle = 6;
+    
+        [SerializeField]
+        public float _minLavaRadius = 4;
+        [SerializeField]
+        public float _maxLavaRadius = 8;
+        
+        
+        [SerializeField]
+        public string _prefabId = "Lava";
+        
+        [SerializeField]
+        private float _minAppearTime = 3;
+        [SerializeField]
+        private float _maxAppearTime = 7;
+        [SerializeField]
+        private float _minDisappearTime = 3;
+        [SerializeField]
+        private float _maxDisappearTime = 7;
+        
+        public int SpawnCountOnCircle => _spawnCountOnCircle;
+        
+        public int SpawnCountStepOnCircle => _spawnCountStepOnCircle;
+        
+        public string PrefabId => _prefabId;
+        
+        
+        public float RandomAppearTime => Random.Range(_minAppearTime, _maxAppearTime); 
+        public float RandomDisappearTime => Random.Range(_minDisappearTime, _maxDisappearTime);
+        
+    }
+}
---
+++ Assets/Scripts/Survivors/WorldEvents/Events/Tornado/TornadoWorldEvent.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Logger.Extension;
+using Survivors.Location;
+using Survivors.Location.Service;
+using Survivors.WorldEvents.Events.Lava;
+using Survivors.WorldEvents.Events.Lava.Config;
+using UnityEngine;
+using Zenject;
+
+namespace Survivors.WorldEvents.Events.Tornado
+{
+    public class TornadoWorldEvent : WorldEvent
+    {
+        private readonly List<> _createdLava = new List<>();
+
+        [Inject]
+        private WorldObjectFactory _worldObjectFactory;
+        [Inject]
+        private World _world;
+
+        private LavaEventConfig _config;
+
+        public override IEnumerator Start(EventConfig config)
+        {
+            this.Logger().Trace("LavaWorldEvent started");
+            _config = (LavaEventConfig) config;
+            var spawner = new CircleLavaSpawner(_config, _world);
+            spawner.SpawnLava(CreateLava);
+            yield return WaitFinish(_config);
+        }
+
+        private void CreateLava(Vector3 place)
+        {
+            var lava = _worldObjectFactory.CreateObject<Lava.Lava>(_config.LavaPrefabId);
+            lava.transform.SetPositionAndRotation(place, Quaternion.identity);
+            lava.Init(_config);
+            _createdLava.Add(lava);
+        }
+
+        protected override void Dispose()
+        {
+            _createdLava.ForEach(it => { it.Dispose(); });
+            _createdLava.Clear();
+        }
+    }
+}
