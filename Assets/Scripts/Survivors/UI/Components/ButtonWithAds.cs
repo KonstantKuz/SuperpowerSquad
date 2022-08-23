691e3067b29416e39475d13d5d13d9e6bbeae736
Konstantin Kuznetsov <k.kuznetsov@inspiritum.com>
2022-08-23 12:47:57 +0300

    add button with ads check

---
+++ Assets/Scripts/Survivors/UI/Components/ButtonWithAds.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using Feofun.UI.Components.Button;
+using Survivors.Advertisment.Service;
+using UnityEngine;
+using Zenject;
+
+namespace Survivors.UI.Components
+{
+    [RequireComponent(typeof(UnityEngine.UI.Button))]
+    public class ButtonWithAds : ActionButton
+    {
+        [SerializeField] private float _checkAdsPeriod = 0.2f;
+        
+        private Coroutine _updateCoroutine;
+        
+        [Inject] private AdsManager _adsManager;
+
+        public bool OverrideInteractable { get; set; }
+        public bool Interactable { get; set; }
+
+        private void OnEnable()
+        {
+            _updateCoroutine = StartCoroutine(UpdateState());
+        }
+
+        private IEnumerator UpdateState()
+        {
+            while (true)
+            {
+                Button.interactable = OverrideInteractable ? Interactable : _adsManager.IsRewardAdsReady();
+                yield return new WaitForSeconds(_checkAdsPeriod);
+            }
+        }
+
+        private void OnDisable()
+        {
+            StopCoroutine(_updateCoroutine);
+            _updateCoroutine = null;
+        }
+    }
+}
