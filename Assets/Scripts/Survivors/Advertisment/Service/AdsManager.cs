76e8c7f1b524d2e9b2af14785c0ecee54ea4bc5c
i.petrenko <i.petrenko@inspiritum.com>
2022-08-23 10:40:40 +0300

    added ads cheat

--- Assets/Scripts/Survivors/Advertisment/Service/AdsManager.cs
+++ Assets/Scripts/Survivors/Advertisment/Service/AdsManager.cs
@@ -1,27 +1,34 @@
 ﻿using System;
+using Logger.Extension;
 using Survivors.Advertisment.Providers;
+using Zenject;
 
 namespace Survivors.Advertisment.Service
 {
     public class AdsManager
     {
-        private readonly IAdsProvider _adsProvider;
-
-        public AdsManager(IAdsProvider adsProvider)
-        {
-            _adsProvider = adsProvider;
+        [Inject]
+        private IAdsProvider _adsProvider;
+        public IAdsProvider AdsProvider 
+        { 
+            get => _adsProvider;
+            set
+            {
+                _adsProvider = value;
+                this.Logger().Info($"IAdsProvider changed, new AdsProvider is {_adsProvider.GetType().Name}");
+            }
         }
         public bool IsRewardAdsReady()
         {
-            return _adsProvider.IsRewardAdsReady();
+            return AdsProvider.IsRewardAdsReady();
         }
         public bool ShowRewardedAds(Action<bool> success)
         {
-            return _adsProvider.ShowRewardedAds(success);
+            return AdsProvider.ShowRewardedAds(success);
         }
         public bool ShowInterstitialAds(Action action, float delay = -1f, bool force = false)
         {
-            return _adsProvider.ShowInterstitialAds(action, delay, force);
+            return AdsProvider.ShowInterstitialAds(action, delay, force);
         }
     }
 }
