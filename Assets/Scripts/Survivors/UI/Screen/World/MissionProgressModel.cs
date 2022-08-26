af86161b5d4ba8bb0c2dc14dade4d02f696f4a42
Konstantin Kuznetsov <k.kuznetsov@inspiritum.com>
2022-08-26 16:18:03 +0300

    create mission progress model

---
+++ Assets/Scripts/Survivors/UI/Screen/World/MissionProgressModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Survivors.Session.Config;
+using Survivors.Session.Service;
+using UniRx;
+
+namespace Survivors.UI.Screen.World
+{
+    public class MissionProgressModel
+    {
+        private const string CHAPTER_LOCALIZATION_ID = "Chapter";
+        private const string SECONDS_LOCALIZATION_ID = "Seconds";
+        
+        private readonly LevelMissionConfig _levelConfig;
+        private readonly IReadOnlyReactiveProperty<float> _playTime;
+        
+        public LevelMissionType MissionType { get; }
+        public string LabelId { get; }
+        public float LeftSeconds => _levelConfig.Time - _playTime.Value;
+        public IReadOnlyReactiveProperty<float> LevelProgress { get; }
+        public MissionProgressModel(LevelMissionConfig levelConfig, 
+            IReadOnlyReactiveProperty<int> killsCount, 
+            IReadOnlyReactiveProperty<float> playTime)
+        {
+            _levelConfig = levelConfig;
+            _playTime = playTime;
+            
+            MissionType = _levelConfig.MissionType;
+            
+            LabelId = _levelConfig.MissionType switch
+            {
+                LevelMissionType.KillCount => CHAPTER_LOCALIZATION_ID,
+                LevelMissionType.Time => SECONDS_LOCALIZATION_ID,
+                _ => throw new ArgumentOutOfRangeException($"Unexpected level mission type := {_levelConfig.MissionType}")
+            };
+            
+            LevelProgress = _levelConfig.MissionType switch
+            {
+                LevelMissionType.KillCount => killsCount.Select(count => (float) count / _levelConfig.KillCount).ToReactiveProperty(),
+                LevelMissionType.Time => playTime.Select(time => time / _levelConfig.Time).ToReactiveProperty(),
+                _ => throw new ArgumentOutOfRangeException($"Unexpected level mission type := {_levelConfig.MissionType}")
+            };
+        }
+    }
+}
---
+++ Assets/Scripts/Survivors/UI/Screen/World/MissionProgressView.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Feofun.UI.Components;
+using SuperMaxim.Messaging;
+using Survivors.Session.Config;
+using Survivors.Session.Messages;
+using Survivors.Session.Service;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace Survivors.UI.Screen.World
+{
+    public class MissionProgressView : MonoBehaviour
+    {
+        [SerializeField] 
+        private ProgressBarView _progressView;
+        [SerializeField]
+        private TextMeshProLocalization _text;
+
+        [Inject]
+        private SessionService _sessionService;
+
+        private MissionProgressModel _model;
+        private CompositeDisposable _disposable;
+
+        public void Init(MissionProgressModel model)
+        {
+            Dispose();
+            _model = model;
+            _disposable = new CompositeDisposable();
+            InitProgressBar();
+            InitText();
+        }
+
+        private void InitProgressBar()
+        {
+            _progressView.Reset(0);
+            _model.LevelProgress.Subscribe(UpdateProgress).AddTo(_disposable);
+        }
+
+        private void UpdateProgress(float progress)
+        {
+            _progressView.SetData(progress);
+        }
+
+        private void InitText()
+        {
+            _text.LocalizationId = _model.LabelId;
+            switch (_model.MissionType)
+            {
+                case LevelMissionType.KillCount:
+                    SetChapterNumber();
+                    break;
+                case LevelMissionType.Time:
+                    SetSecondsToWin();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException($"Unexpected level mission type := {_model.MissionType}");
+            }
+        }
+
+        private void SetChapterNumber()
+        {
+            _text.SetTextFormatted(_text.LocalizationId, _sessionService.LevelConfig.Level);
+        }
+
+        private void SetSecondsToWin()
+        {
+            _text.SetTextFormatted(_text.LocalizationId, _model.LeftSeconds);
+            _model.LevelProgress.Subscribe(it => UpdateTimerText(_model.LeftSeconds)).AddTo(_disposable);
+        }
+        
+        private void UpdateTimerText(float leftSeconds)
+        {
+            _text.SetTextFormatted(_text.LocalizationId, leftSeconds);
+        }
+
+        private void Dispose()
+        {
+            _disposable?.Dispose();
+            _disposable = null;
+        }
+
+        private void OnDisable()
+        {
+            Dispose();
+        }
+    }
+}
