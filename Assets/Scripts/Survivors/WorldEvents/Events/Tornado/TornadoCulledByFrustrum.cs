01bb8b708c0ae3aa51d70f5598c76ce19249f1c5
Anatoly Ahmedov <ludd@bk.ru>
2022-08-26 19:08:51 +0300

    add frustrum culling to tornado

---
+++ Assets/Scripts/Survivors/WorldEvents/Events/Tornado/TornadoCulledByFrustrum.cs
@@ -0,0 +1,38 @@
+﻿using Survivors.Units.Component.FrustrumCulling;
+using UnityEngine;
+using Zenject;
+
+namespace Survivors.Camera.FrustrumCulling
+{
+    [RequireComponent(typeof(Collider))]
+    public class TornadoCulledByFrustrum : MonoBehaviour, ICulledByFrustrum
+    {
+        [SerializeField] private GameObject _rootVfx;
+
+        private Collider _collider;
+        
+        [Inject] private FrustrumCullingSystem _cullingSystem;
+
+        public void SetVisible(bool isVisible)
+        {
+            _rootVfx.SetActive(isVisible);
+        }
+
+        public Bounds Bounds => _collider.bounds;
+
+        private void Awake()
+        {
+            _collider = GetComponent<Collider>();
+        }
+
+        private void OnEnable()
+        {
+            _cullingSystem.Add(this);
+        }
+
+        private void OnDisable()
+        {
+            _cullingSystem.Remove(this);
+        }
+    }
+}
