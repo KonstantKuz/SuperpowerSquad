b56b62fb6b4f58fe954bca2e24a318a0dc8ac9c8
Konstantin Kuznetsov <k.kuznetsov@inspiritum.com>
2022-08-29 14:09:18 +0300

    add attack animation handling

--- Assets/Scripts/Survivors/Units/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Survivors/Units/Enemy/EnemyAttack.cs
@@ -1,8 +1,10 @@
 ﻿using Feofun.Components;
+using JetBrains.Annotations;
 using Logger.Extension;
 using Survivors.Extension;
 using Survivors.Units.Component.Health;
 using Survivors.Units.Enemy.Model;
+using Survivors.Units.Player.Attack;
 using Survivors.Units.Weapon;
 using UnityEngine;
 
@@ -11,49 +13,62 @@ namespace Survivors.Units.Enemy
     [RequireComponent(typeof(EnemyAi))]
     public class EnemyAttack : MonoBehaviour, IInitializable<IUnit>, IUpdatableComponent
     {
+        private static readonly int AttackHash = Animator.StringToHash("Attack");
+
         private EnemyAi _enemyAi;
         private BaseWeapon _weapon;
         private EnemyAttackModel _attackModel;
+        private WeaponTimer _weaponTimer;
+        
+        private Animator _animator;
+        [CanBeNull]
+        private WeaponAnimationHandler _weaponAnimationHandler;
 
-        private float _attackTimer;
-
+        private bool HasWeaponAnimationHandler => _weaponAnimationHandler != null;
         
         public void Init(IUnit unit)
         {
             _attackModel = (EnemyAttackModel) unit.Model.AttackModel;
+            _weaponTimer = new WeaponTimer(_attackModel.AttackInterval);
+            _weaponTimer.OnAttackReady += Attack;
+            if (HasWeaponAnimationHandler) {
+                _weaponAnimationHandler.OnFireEvent += Fire;
+            }
         }
 
         private void Awake()
         {
             _enemyAi = gameObject.RequireComponent<EnemyAi>();
             _weapon = gameObject.RequireComponentInChildren<BaseWeapon>();
+            _animator = gameObject.RequireComponentInChildren<Animator>();
+            _weaponAnimationHandler = gameObject.GetComponentInChildren<WeaponAnimationHandler>();
         }
 
         public void OnTick()
         {
-            UpdateTimer();
-            if (CanAttack())
-            {
-                Attack();
-            }
-        }
-
-        private void UpdateTimer()
-        {
-            _attackTimer += Time.deltaTime;
+            _weaponTimer.OnTick();
         }
 
         private bool CanAttack()
         {
-            return _enemyAi.CurrentTarget != null 
-                   && _enemyAi.DistanceToTarget <= _attackModel.AttackDistance 
-                   && _attackTimer >= _attackModel.AttackInterval.Value;
+            return _enemyAi.CurrentTarget != null && 
+                   _enemyAi.DistanceToTarget <= _attackModel.AttackDistance;
         }
         
         private void Attack()
         {
+            _animator.SetTrigger(AttackHash);
+            if (!HasWeaponAnimationHandler) {
+                Fire();
+            }
+        }
+
+        private void Fire()
+        {
+            if (!CanAttack()) {
+                return;
+            }
             _weapon.Fire(_enemyAi.CurrentTarget, null, DoDamage);
-            _attackTimer = 0;
         }
 
         private void DoDamage(GameObject target)
