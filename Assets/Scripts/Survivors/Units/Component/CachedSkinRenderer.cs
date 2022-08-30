dc9c552748621b4d5a00fcc1fe1a08ecba6ab7e5
Anatoly Ahmedov <ludd@bk.ru>
2022-08-30 11:35:43 +0300

    optimize skinning

---
+++ Assets/Scripts/Survivors/Units/Component/CachedSkinRenderer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Survivors.Units.Component
+{
+    public class CachedSkinRenderer : MonoBehaviour
+    {
+        private const int FRAME_COUNT = 30;
+        
+        [SerializeField] private Animator _animator;
+        [SerializeField] private SkinnedMeshRenderer _renderer;
+
+        private int _animationOffset;
+
+        private static readonly Dictionary<int, Mesh> _meshCache = new Dictionary<int, Mesh>();
+        public Bounds Bounds => _renderer.bounds;
+
+        private void Awake()
+        {
+            _renderer.updateWhenOffscreen = true;
+            _animator.cullingMode = AnimatorCullingMode.AlwaysAnimate;
+            
+            var animState = _animator.GetCurrentAnimatorStateInfo(0);
+            for (int frameNum = 0; frameNum <= FRAME_COUNT; frameNum++)
+            {
+                _animator.Update(animState.length / FRAME_COUNT);
+                _meshCache[frameNum] = new Mesh();
+                
+                _renderer.BakeMesh(_meshCache[frameNum]);
+            }
+
+            _renderer.updateWhenOffscreen = false;
+            _renderer.enabled = false;
+            _animator.cullingMode = AnimatorCullingMode.AlwaysAnimate;
+
+            _animationOffset = Random.Range(0, FRAME_COUNT);
+        }
+
+        private void LateUpdate()
+        {
+            var animState = _animator.GetCurrentAnimatorStateInfo(0);
+            int currentFrame = Mathf.RoundToInt(  animState.normalizedTime * FRAME_COUNT + _animationOffset) % (FRAME_COUNT + 1);
+            Graphics.DrawMesh(_meshCache[currentFrame], 
+                transform.localToWorldMatrix, 
+                _renderer.material, 
+                gameObject.layer, 
+                UnityEngine.Camera.main, 
+                0, 
+                null, 
+                _renderer.shadowCastingMode, 
+                _renderer.receiveShadows,
+                gameObject.transform,
+                _renderer.lightProbeUsage);
+        }
+
+        private void OnEnable()
+        {
+            _animator.cullingMode = AnimatorCullingMode.AlwaysAnimate;
+        }
+
+        private void OnDisable()
+        {
+            _animator.cullingMode = AnimatorCullingMode.CullCompletely;
+        }
+    }
+}
---
+++ Assets/Scripts/Survivors/Units/Component/CachedSkinRendererCulling.cs
@@ -0,0 +1,36 @@
+﻿using Survivors.Units.Component.FrustrumCulling;
+using UnityEngine;
+using Zenject;
+
+namespace Survivors.Units.Component
+{
+    [RequireComponent(typeof(CachedSkinRenderer))]
+    public class CachedSkinRendererCulling : MonoBehaviour, ICulledByFrustrum
+    {
+        private CachedSkinRenderer _cachedSkinRenderer;
+        
+        [Inject] private FrustrumCullingSystem _cullingSystem;
+
+        private void Awake()
+        {
+            _cachedSkinRenderer = GetComponent<CachedSkinRenderer>();
+        }
+
+        public void SetVisible(bool isVisible)
+        {
+            _cachedSkinRenderer.enabled = isVisible;
+        }
+
+        public Bounds Bounds => _cachedSkinRenderer.Bounds;
+
+        private void OnEnable()
+        {
+            _cullingSystem.Add(this);
+        }
+
+        private void OnDisable()
+        {
+            _cullingSystem.Remove(this);
+        }
+    }
+}
