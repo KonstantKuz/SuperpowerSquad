d3ab19176933bc287b59f29f0241f29c9231d909
i.petrenko <i.petrenko@inspiritum.com>
2022-09-02 13:42:51 +0300

    fixed destroy

--- Assets/Scripts/Survivors/Units/Component/Death/DeathAnimation.cs
+++ Assets/Scripts/Survivors/Units/Component/Death/DeathAnimation.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using DG.Tweening;
-using Feofun.Components;
 using Survivors.Location.ObjectFactory;
-using Survivors.Location.ObjectFactory.Factories;
 using UnityEngine;
 using Zenject;
 
@@ -23,6 +21,8 @@ namespace Survivors.Units.Component.Death
         private Tweener _disappearTween;
         private Coroutine _disappear;
         
+        [Inject(Id = ObjectFactoryType.Instancing)]
+        private IObjectFactory _objectFactory;
         private void Awake()
         {
             _animator = GetComponentInChildren<Animator>();
@@ -41,7 +41,7 @@ namespace Survivors.Units.Component.Death
             yield return new WaitForSeconds(_delayUntilDisappear);
             _disappearTween = gameObject.transform.DOMoveY(transform.position.y - _offsetYDisappear, _disappearTime);
             yield return _disappearTween.WaitForCompletion(); 
-            Destroy(gameObject);
+            _objectFactory.Destroy(gameObject);
         }
 
         private void EndAnimationIfStarted()
